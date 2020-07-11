041e30df4ffab58f60130b22c033feb417e017d9
Clemens <clemensbrauer@web.de>
2020-07-11 14:23:23 +0200

    feat: add xml_from_good_to_with_mask

--- 2. Datenbeschaffung/2.1 xml_From_good_to_with_mask.py
+++ 2. Datenbeschaffung/2.1 xml_From_good_to_with_mask.py
@@ -1,43 +1,33 @@
 #!/usr/bin/env python3
 # -*- coding: utf-8 -*-
-"""
-Created on Thu Jun 25 11:16:01 2020
-
-@author: swift
-"""
-
 
 import xml.etree.ElementTree as et
-
 import os
 
+# Pfad in welchem sich die XML Dateien befinden
+anno_directory = r'path/to/xml/files'
 
+# Funktion schreibt die Bounding Box Bezeichnung von good in with_mask und bad in without_mask um.
+def rewrite_annotation(anno_directory):
 
-anno_directory = r'/Users/swift/Downloads/527030-966454-bundle-archive/labels/'
-
-
-
-
-
-def rotate_img(anno_directory):
+	# für jede Datei im Ordner
     for entry in os.scandir(anno_directory):
+    
+    	# Falls es sich um eine XML Datei handelt
         if (entry.path.endswith(".xml") and entry.is_file()):
-            
-
-            
             tree = et.parse(entry.path)
             root = tree.getroot()
-            
+			
+			# Für jedes annotierte Objekt 
             for boxes in root.iter('object'):
-                
                 if boxes.find("name").text == "good":
                     boxes.find("name").text = "with_mask"
                 else :
                     boxes.find("name").text = "without_mask"
-            
-               
+                    
+            # Schreib Änderung in die Datei
             tree.write(entry.path)
 
 
-
-rotate_img(anno_directory)
+# führe Funktion aus
+rewrite_annotation(anno_directory)
